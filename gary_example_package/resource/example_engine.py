26f3a0d3471a0ff4323b19f038a01c27e3c65bba
Kai Geronik <kai@unlimited-robotics.com>
2024-12-09 17:55:55 +0200

    Fixed typo

--- gary_example_package/resource/example_engine.py
+++ gary_example_package/resource/example_engine.py
@@ -6,13 +6,13 @@ from rclpy.node import Node
 class ExampleEngine(Node):
     def __init__(self):
         super().__init__('example_engine')
-        self.get_logger().info('')
+        self.get_logger().info('='*50)
         self.get_logger().info('Example Engine Node has been started!')
         self.get_logger().info('Optional commands:')
         self.get_logger().info('  - teleop_keyboard')
         self.get_logger().info('  - leds')
         self.get_logger().info('  - easter')
-        self.get_logger().info('')
+        self.get_logger().info('='*50)
 
 
 def main(args=None):
